a72b64717447168edf384bb387850a4fd66a1e60
Gustav Behm <gustav.behm@iota.org>
2021-01-12 12:07:56 +0100

    Add changelog message and document the Option<_> usage in the X25519 function

--- src/x25519.rs
+++ src/x25519.rs
@@ -21,6 +21,11 @@ static BASE_POINT: PublicKey = [
 // scalar/u  secret/public
 // X25519    Curve25519
 
+/// Perform the X25519 function on the given secret key and public key
+///
+/// When the public key is omitted the curves base point is used, hence this function can be used
+/// to derive the public key of a Curve25519 secret key as well as performing a Diffie-Hellman
+/// exchange between a secret key and another public key.
 pub fn X25519(s: &SecretKey, u: Option<&PublicKey>) -> PublicKey {
     x25519_dalek::x25519(*s, *u.unwrap_or(&BASE_POINT))
 }
