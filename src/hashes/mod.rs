8cf3d791e1409ac51e360231ca821cc9f596d255
l1h3r <devin.turner@iota.org>
2021-02-16 10:51:28 -0800

    Expose Digest trait and Output type

--- src/hashes/mod.rs
+++ src/hashes/mod.rs
@@ -6,3 +6,11 @@ pub mod curl_p;
 
 #[cfg(feature = "sha")]
 pub mod sha;
+
+#[cfg(feature = "digest")]
+#[doc(inline)]
+pub use digest::Digest;
+
+#[cfg(feature = "digest")]
+#[doc(inline)]
+pub use digest::Output;
