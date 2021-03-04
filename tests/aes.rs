72c8532d1b2de99da7a2c65edf4b15dc6ac2afae
l1h3r <devin.turner@iota.org>
2021-03-04 13:46:25 -0800

    Separate all fixtures and tests

--- tests/aes.rs
+++ tests/aes.rs
@@ -15,18 +15,8 @@ struct TestVector {
 }
 
 #[test]
-fn test_vectors_AES_256_GCM() -> crypto::Result<()> {
-    let tvs = [
-        // https://csrc.nist.gov/Projects/Cryptographic-Algorithm-Validation-Program/CAVP-TESTING-BLOCK-CIPHER-MODES#GCMVS
-        TestVector {
-            key: "83688deb4af8007f9b713b47cfa6c73e35ea7a3aa4ecdb414dded03bf7a0fd3a",
-            iv: "0b459724904e010a46901cf3",
-            associated_data: "794a14ccd178c8ebfd1379dc704c5e208f9d8424",
-            plaintext: "33d893a2114ce06fc15d55e454cf90c3",
-            ciphertext: "cc66bee423e3fcd4c0865715e9586696",
-            tag: "0fb291bd3dba94a1dfd8b286cfb97ac5",
-        },
-    ];
+fn test_vectors_aes_256_gcm() -> crypto::Result<()> {
+    let tvs = include!("fixtures/aes_256_gcm.rs");
 
     for tv in tvs.iter() {
         let mut key = [0; AES_256_GCM::KEY_LENGTH];
---
+++ tests/fixtures/aes_256_gcm.rs
@@ -0,0 +1,11 @@
+[
+    // https://csrc.nist.gov/Projects/Cryptographic-Algorithm-Validation-Program/CAVP-TESTING-BLOCK-CIPHER-MODES#GCMVS
+    TestVector {
+        key: "83688deb4af8007f9b713b47cfa6c73e35ea7a3aa4ecdb414dded03bf7a0fd3a",
+        iv: "0b459724904e010a46901cf3",
+        associated_data: "794a14ccd178c8ebfd1379dc704c5e208f9d8424",
+        plaintext: "33d893a2114ce06fc15d55e454cf90c3",
+        ciphertext: "cc66bee423e3fcd4c0865715e9586696",
+        tag: "0fb291bd3dba94a1dfd8b286cfb97ac5",
+    },
+]
