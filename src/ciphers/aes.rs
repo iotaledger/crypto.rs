61924d5c237254f5a6da0e9ac61cd7e7bb310058
nothingismagick <denjell@mailscript.com>
2021-03-10 10:42:32 +0100

    hotfix(typo)

--- src/ciphers/aes.rs
+++ src/ciphers/aes.rs
@@ -4,4 +4,4 @@
 use crate::ciphers::traits::consts::{U12, U16, U32};
 
 pub type Aes256Gcm = aes_gcm::Aes256Gcm;
-impl_aead!(Aes256Gcm, "AES-128-GCM", U32, U12, U16);
+impl_aead!(Aes256Gcm, "AES-256-GCM", U32, U12, U16);
