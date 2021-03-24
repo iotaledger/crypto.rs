aa3c6498b7fadfb1c693278574ba7da0be86c3a1
l1h3r <devin.turner@iota.org>
2021-03-24 11:26:59 -0700

    Add ChaCha20Poly1305

--- src/ciphers/chacha.rs
+++ src/ciphers/chacha.rs
@@ -1,7 +1,10 @@
 // Copyright 2020 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::ciphers::traits::consts::{U16, U24, U32};
+use crate::ciphers::traits::consts::{U12, U16, U24, U32};
+
+pub type ChaCha20Poly1305 = chacha20poly1305::ChaCha20Poly1305;
+impl_aead!(ChaCha20Poly1305, "CHACHA20-POLY1305", U32, U12, U16);
 
 pub type XChaCha20Poly1305 = chacha20poly1305::XChaCha20Poly1305;
 impl_aead!(XChaCha20Poly1305, "XCHACHA20-POLY1305", U32, U24, U16);
