a8c96bcfa7be16386534cb20b073eab23c824e0d
Thibault Martinez <thibault@iota.org>
2021-04-15 10:08:16 +0200

    Deprecate kerl module

--- src/hashes/sponge/mod.rs
+++ src/hashes/sponge/mod.rs
@@ -9,6 +9,7 @@ pub mod curl_p;
 
 #[cfg(feature = "kerl")]
 #[cfg_attr(docsrs, doc(cfg(feature = "kerl")))]
+#[deprecated]
 pub mod kerl;
 
 /// The length of a hash in units of balanced trits.
--- tests/kerl.rs
+++ tests/kerl.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #![cfg(feature = "kerl")]
+#![allow(deprecated)]
 
 use crypto::hashes::sponge::{kerl::Kerl, Sponge};
 
