2b50ff843b0596aca63083922612e89339bd5232
Thibault Martinez <thibault@iota.org>
2021-04-18 15:50:40 +0200

    Add Hash struct

--- src/hashes/ternary/mod.rs
+++ src/hashes/ternary/mod.rs
@@ -17,7 +17,7 @@ pub const HASH_LENGTH: usize = 243;
 /// The length of a hash in trytes.
 pub const HASH_LENGTH_TRYTES: usize = 243 / 3;
 
-use bee_ternary::{TritBuf, Trits};
+use bee_ternary::{raw::RawEncoding, Btrit, TritBuf, Trits, T1B1};
 
 use core::ops::DerefMut;
 
@@ -72,5 +72,91 @@ impl<T: Sponge, U: DerefMut<Target = T>> Sponge for U {
 
     fn squeeze_into(&mut self, buf: &mut Trits) -> Result<(), Self::Error> {
         T::squeeze_into(self, buf)
+    }
+}
+
+#[derive(Debug)]
+pub enum HashError {
+    WrongLength,
+}
+
+/// Ternary cryptographic hash.
+#[derive(Copy, Clone)]
+pub struct Hash([Btrit; HASH_LENGTH]);
+
+impl Hash {
+    /// Creates a hash filled with zeros.
+    pub fn zeros() -> Self {
+        Self([Btrit::Zero; HASH_LENGTH])
+    }
+
+    /// Interpret the `Hash` as a trit slice.
+    pub fn as_trits(&self) -> &Trits<T1B1> {
+        &*self
+    }
+
+    /// Interpret the `Hash` as a mutable trit slice.
+    pub fn as_trits_mut(&mut self) -> &mut Trits<T1B1> {
+        &mut *self
+    }
+
+    /// Returns the weight - number of ending 0s - of the `Hash`.
+    #[allow(clippy::cast_possible_truncation)] // `HASH_LENGTH` is smaller than `u8::MAX`.
+    pub fn weight(&self) -> u8 {
+        self.iter().rev().take_while(|t| *t == Btrit::Zero).count() as u8
+    }
+}
+
+impl<'a, T: RawEncoding<Trit = Btrit>> core::convert::TryFrom<&'a Trits<T>> for Hash {
+    type Error = HashError;
+
+    fn try_from(trits: &'a Trits<T>) -> Result<Self, Self::Error> {
+        if trits.len() == HASH_LENGTH {
+            let mut hash = Self([Btrit::Zero; HASH_LENGTH]);
+            hash.copy_from(trits);
+            Ok(hash)
+        } else {
+            Err(HashError::WrongLength)
+        }
+    }
+}
+
+impl core::ops::Deref for Hash {
+    type Target = Trits<T1B1>;
+
+    fn deref(&self) -> &Trits<T1B1> {
+        <&Trits>::from(&self.0 as &[_])
+    }
+}
+
+impl DerefMut for Hash {
+    fn deref_mut(&mut self) -> &mut Trits<T1B1> {
+        <&mut Trits>::from(&mut self.0 as &mut [_])
+    }
+}
+
+impl PartialEq for Hash {
+    fn eq(&self, other: &Self) -> bool {
+        self.as_trits() == other.as_trits()
+    }
+}
+
+impl Eq for Hash {}
+
+impl core::fmt::Display for Hash {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        core::fmt::Debug::fmt(self, f)
+    }
+}
+
+impl core::fmt::Debug for Hash {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "{:?}", self.as_trits())
+    }
+}
+
+impl core::hash::Hash for Hash {
+    fn hash<H: core::hash::Hasher>(&self, hasher: &mut H) {
+        self.0.hash(hasher)
     }
 }
