8c39eb07448ce1503accd6845c1242ce2fdf8cde
Alexandre Dang <alexandre.dang@iota.org>
2021-10-19 18:13:42 +0200

    Add BLAKE2b-160   - Additional wrapper for VarBlake2b   - Additional tests for 160 bits length   - Update list of supported algorithms

--- src/hashes/blake2b.rs
+++ src/hashes/blake2b.rs
@@ -4,7 +4,7 @@
 use blake2::VarBlake2b;
 use digest::{
     generic_array::{
-        typenum::{Unsigned, U32},
+        typenum::{Unsigned, U20, U32},
         GenericArray,
     },
     FixedOutput, Reset, Update, VariableOutput,
@@ -46,6 +46,47 @@ impl Reset for Blake2b256 {
 }
 
 impl Update for Blake2b256 {
+    fn update(&mut self, data: impl AsRef<[u8]>) {
+        self.0.update(data);
+    }
+}
+
+/// Blake2b instance with a 160-bit output.
+#[derive(Clone)]
+pub struct Blake2b160(VarBlake2b);
+
+impl Blake2b160 {
+    /// Creates a new [`Blake2b160`] instance.
+    pub fn new() -> Self {
+        Self(VarBlake2b::new_keyed(&[], U20::USIZE))
+    }
+}
+
+impl Default for Blake2b160 {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl FixedOutput for Blake2b160 {
+    type OutputSize = U20;
+
+    fn finalize_into(self, out: &mut GenericArray<u8, Self::OutputSize>) {
+        self.0.finalize_variable(|output| out.copy_from_slice(output));
+    }
+
+    fn finalize_into_reset(&mut self, out: &mut GenericArray<u8, Self::OutputSize>) {
+        self.0.finalize_variable_reset(|output| out.copy_from_slice(output));
+    }
+}
+
+impl Reset for Blake2b160 {
+    fn reset(&mut self) {
+        self.0.reset();
+    }
+}
+
+impl Update for Blake2b160 {
     fn update(&mut self, data: impl AsRef<[u8]>) {
         self.0.update(data);
     }
