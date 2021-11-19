7915c7fddd85e06d3424cce4af858c99895eb618
Thibault Martinez <thibault@iota.org>
2021-11-19 13:06:43 +0100

    Make curl_p module no_std

--- src/hashes/ternary/curl_p/batched/bct.rs
+++ src/hashes/ternary/curl_p/batched/bct.rs
@@ -1,7 +1,10 @@
 // Copyright 2020-2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use std::ops::{Deref, DerefMut, Range};
+extern crate alloc;
+
+use alloc::vec::Vec;
+use core::ops::{Deref, DerefMut, Range};
 
 #[derive(Clone, Copy, Debug)]
 pub(crate) struct BcTrit(pub(crate) usize, pub(crate) usize);
--- src/hashes/ternary/curl_p/batched/mod.rs
+++ src/hashes/ternary/curl_p/batched/mod.rs
@@ -6,6 +6,8 @@
 mod bct;
 mod bct_curlp;
 
+extern crate alloc;
+
 use crate::hashes::ternary::HASH_LENGTH;
 
 use bct::{BcTrit, BcTritArr, BcTritBuf};
@@ -16,8 +18,10 @@ use bee_ternary::{
     Btrit, TritBuf,
 };
 
+use alloc::vec::Vec;
+
 /// The number of inputs that can be processed in a single batch.
-pub const BATCH_SIZE: usize = 8 * std::mem::size_of::<usize>();
+pub const BATCH_SIZE: usize = 8 * core::mem::size_of::<usize>();
 const HIGH_BITS: usize = usize::max_value();
 const NUM_ROUNDS: usize = 81;
 
@@ -166,7 +170,7 @@ where
 /// A helper iterator type for the output of the `hash_batched` method.
 struct BatchedHashes<'a, B: RawEncodingBuf> {
     hasher: &'a mut CurlPBatchHasher<B>,
-    range: std::ops::Range<usize>,
+    range: core::ops::Range<usize>,
 }
 
 impl<'a, B> Iterator for BatchedHashes<'a, B>
